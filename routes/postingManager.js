1f521b69740739713f7491920f41e6a2e9e3fe54
NickF <lkbjhgfd@hotmail.com>
2016-03-30 23:44:47 -0400

    link everything together

--- routes/postingManager.js
+++ routes/postingManager.js
@@ -1,11 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
+var isAuthenticated = function (req, res, next) {
+	// if user is authenticated in the session, call the next() to call the next request handler 
+	// Passport adds this method to request object. A middleware is allowed to add properties to
+	// request and response objects
+	if (req.isAuthenticated())
+		return next();
+	// if the user is not authenticated then redirect him to the login page
+	res.redirect('/');
+}
+
+
 var mongoose = require('mongoose');
 var Posting = require('../models/posting.js');
 
 //get all postings belong to a user
-router.get('/', function(req, res, next) {
+router.get('/', isAuthenticated, function(req, res, next) {
   Posting.find({
 	    '_id': { $in: req.user.projects}
 	           },  function (err, postings) {
@@ -16,7 +27,9 @@ router.get('/', function(req, res, next) {
   });
 });
 
-router.get('/testPosting', function(req, res, next) {
+
+//DONOT CALL IT
+router.get('/testPosting', isAuthenticated, function(req, res, next) {
 	Posting.create({
 		title: 'RAD dev',
 	    description: 'dev a project',
@@ -36,7 +49,7 @@ router.get('/testPosting', function(req, res, next) {
 	});
 
 //go to the post creation page
-router.get('/createpost', function(req, res){
+router.get('/createpost', isAuthenticated,  function(req, res){
 	res.render('addpost',{message: req.flash('message')});
 });
 
@@ -56,6 +69,15 @@ router.post('/createpost', function(req, res, next) {
     res.json(post);
   });
 });
+
+//Delete a post
+router.get('/deletepost', isAuthenticated, function(req, res, next) {
+	  console.log(req.query.id);
+	  Posting.findByIdAndRemove(req.query.id, req.body, function (err, post) {
+	    if (err) return next(err);
+	    res.json(post);
+	  });
+	});
 
 //show posting by id
 router.get('/:id', function(req, res, next) {
