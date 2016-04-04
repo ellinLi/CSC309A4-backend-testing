f2667cb815a41e23e4448a41874b3519061f1528
Manke Luo <luo.manke@hotmail.com>
2016-04-03 22:05:00 -0400

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,59 +1,63 @@
 var express = require('express');
 var router = express.Router();
 
-
-var isAuthenticated = function (req, res, next) {
-	// if user is authenticated in the session, call the next() to call the next request handler 
-	// Passport adds this method to request object. A middleware is allowed to add properties to
-	// request and response objects
-	if (req.isAuthenticated())
-		return next();
-	// if the user is not authenticated then redirect him to the login page
-	res.redirect('/');
+var isAuthenticated = function(req, res, next) {
+    // if user is authenticated in the session, call the next() to call the next request handler 
+    // Passport adds this method to request object. A middleware is allowed to add properties to
+    // request and response objects
+    if (req.isAuthenticated())
+        return next();
+    // if the user is not authenticated then redirect him to the login page
+    res.redirect('/');
 }
 
-module.exports = function(passport){
+module.exports = function(passport) {
 
-	/* GET login page. */
-	router.get('/', function(req, res) {
-    	// Display the Login page with any flash message, if any
-		res.render('index', { message: req.flash('message') });
-	});
+    /* GET login page. */
+    router.get('/', function(req, res, next) {
+        // console.log(postings);
+        res.render('find_project');
+    });
 
-	/* Handle Login POST */
-	router.post('/login', passport.authenticate('login', {
-		successRedirect: '/home',
-		failureRedirect: '/',
-		failureFlash : true  
-	}));
+    router.get('/project_detail.html', function(req, res, next) {
+        // console.log(postings);
+        res.render('project_detail');
+    });
 
-	/* GET Registration Page */
-	router.get('/signup', function(req, res){
-		res.render('register',{message: req.flash('message')});
-	});
+    router.get('/create_project.html', function(req, res, next) {
+        // console.log(postings);
+        res.render('create_project');
+    });
 
-	/* Handle Registration POST */
-	router.post('/signup', passport.authenticate('signup', {
-		successRedirect: '/home',
-		failureRedirect: '/signup',
-		failureFlash : true  
-	}));
+    /* Handle Login POST */
+    router.post('/login', passport.authenticate('login', {
+        successRedirect: '/create_project',
+        failureRedirect: '/',
+        failureFlash: true
+    }));
 
-	/* GET Home Page */
-	router.get('/home', isAuthenticated, function(req, res){
-		res.render('home', { user: req.user });
-	});
+    /* GET Registration Page */
+    router.get('/signup', function(req, res) {
+        res.render('register', { message: req.flash('message') });
+    });
 
-	/* Handle Logout */
-	router.get('/signout', function(req, res) {
-		req.logout();
-		res.redirect('/');
-	});
+    /* Handle Registration POST */
+    router.post('/signup', passport.authenticate('signup', {
+        successRedirect: '/home',
+        failureRedirect: '/signup',
+        failureFlash: true
+    }));
 
-	return router;
+    /* GET Home Page */
+    router.get('/home', isAuthenticated, function(req, res) {
+        res.render('find_project', { user: req.user });
+    });
+
+    /* Handle Logout */
+    router.get('/signout', function(req, res) {
+        req.logout();
+        res.redirect('/');
+    });
+
+    return router;
 }
-
-
-
-
-
